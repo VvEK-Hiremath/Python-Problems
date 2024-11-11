1ca95acd144f067a8ff1a37387919929ffc77355
Vivekanand <vkhiremath368@gmail.com>
2024-11-11 09:37:58 +0530

    Update Sql_practice.sql

--- SQL/Sql_practice.sql
+++ SQL/Sql_practice.sql
@@ -26,3 +26,10 @@ GROUP BY tweet_count_per_user;
 --for laptops as laptop_reviews and the total viewership for mobile devices as
 --mobile_views.
 
+SELECT
+SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS
+laptop_views,
+SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END)
+AS mobile_views
+FROM viewership;
+
