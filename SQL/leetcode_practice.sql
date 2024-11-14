24189abcba0c9d51c9ee689a2890f1dac4c26e23
Vivekanand <vkhiremath368@gmail.com>
2024-11-14 13:11:13 +0530

    Update leetcode_practice.sql

--- SQL/leetcode_practice.sql
+++ SQL/leetcode_practice.sql
@@ -70,3 +70,12 @@ SELECT
         ELSE NULL
     END AS SecondHighestSalary
 FROM Employee;
+
+
+--average project experience
+
+select project_id, round(avg(experience_years),2) as average_years
+from project a left join
+employee b on a.employee_id = b.employee_id
+group by project_id
+
