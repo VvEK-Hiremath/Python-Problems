3de78b0f40458258d371ded10b9b04df1c83cd37
Vivekanand <vkhiremath368@gmail.com>
2024-11-18 12:12:10 +0530

    Update aggr_practice.sql

--- SQL/aggr_practice.sql
+++ SQL/aggr_practice.sql
@@ -17,3 +17,21 @@ sum(
 ) as approved_total_amount
  from transactions
  group by month,country
+
+
+--1174. Immediate Food Delivery II
+with cte as
+(
+    select delivery_id, customer_id, order_date as od,
+    customer_pref_delivery_date as cpd,
+    row_number() over(partition by customer_id order by order_date,delivery_id) as rn
+    from delivery
+)
+
+select round(count(
+   case
+    when od = cpd then 1
+    else null
+    end
+)/count(delivery_id),4)*100 as immediate_percentage 
+from cte where rn = 1
